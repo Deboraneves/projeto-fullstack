18e7d3e1bb87f83d8b176c61944d12d3a9428889
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2022-11-06 14:04:12 -0300

    método DELETE

--- js/index.js
+++ js/index.js
@@ -1,4 +1,56 @@
-const atualizaTudo = (item) => {
+// MÉTODO POST
+const adicionarCarrinho = (produto) => {
+    const item = produto.parentNode.parentNode.parentNode
+
+    //Informações do produto
+    let nome = item.querySelector('.title').innerText
+    let imagem = item.querySelector('img').getAttribute('src')
+    let valor = item.querySelector('.value').innerText
+    valor = valor.split(' ')[1]
+    let quantidade = 1
+
+    $.ajax({
+        method: "POST",
+        url: `http://localhost:9000/carrinho`,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        cache: false,
+        dataType: "json",
+        data: JSON.stringify({
+            produto: nome,
+            preco: parseFloat(valor),
+            quantidade: quantidade,
+            imagem: imagem
+        }),
+        complete: () => {
+            location.reload()
+        }
+    })
+}
+
+const excluiProdutoCarrinho = (produto) => {
+    const elemento = produto.parentNode.parentNode
+    let id = parseInt(elemento.id)
+
+    $.ajax({
+        method: "DELETE",
+        url: `http://localhost:9000/carrinho/${id}`,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        cache: false,
+        complete: () => {
+            
+        }
+    })
+
+}
+
+// MÉTODO PUT
+const atualizaQuantidade = (item) => {
     const e = item.parentNode.parentNode
 
     const id = parseInt(e.id)
@@ -10,9 +62,6 @@ const atualizaTudo = (item) => {
     const total = valor * quantidade
     e.querySelector('.total').innerText = total
 
-    /**
-     * Preparando a requisição pro Back-End
-     */
     $.ajax({
         method: "PUT",
         url: `http://localhost:9000/carrinho`,
@@ -36,45 +85,9 @@ const atualizaTudo = (item) => {
 
 }
 
-const adicionarCarrinho = (produto) => {
-    const item = produto.parentNode.parentNode.parentNode
-
-    /**
-     * Informações do produto
-     */
-    let nome = item.querySelector('.title').innerText
-    let imagem = item.querySelector('img').getAttribute('src')
-    let valor = item.querySelector('.value').innerText
-    valor = valor.split(' ')[1]
-    let quantidade = 1
-
-
-    /**
-     * Preparando a requisição pro Back-End
-     */
-    $.ajax({
-        method: "POST",
-        url: `http://localhost:9000/carrinho`,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        cache: false,
-        dataType: "json",
-        data: JSON.stringify({
-            produto: nome,
-            preco: parseFloat(valor),
-            quantidade: quantidade,
-            imagem: imagem
-        }),
-        complete: () => {
-            location.reload()
-        }
-    })
-}
-
 jQuery(function () {
 
+    // MÉTODO GET
     $.get("http://localhost:9000/carrinho", function (data) {
         var dados = data
 
@@ -88,11 +101,11 @@ jQuery(function () {
                 <td class='nome'>${data[i].produto}</td>
                 <td>R$ ${data[i].preco}</td>
                 <td class="qty">
-                    <input type="text" class="form-control" onfocusout="atualizaTudo(this)" value="${data[i].quantidade}">
+                    <input type="text" class="form-control" onfocusout="atualizaQuantidade(this)" value="${data[i].quantidade}">
                 </td>
                 <td class="total">R$ ${data[i].preco}</td>
                 <td>
-                  <a href="#" class="btn btn-danger btn-sm">
+                  <a href="#" class="btn btn-danger btn-sm" onclick="excluiProdutoCarrinho(this)">
                     <i class="fa fa-times"></i>
                   </a>
                 </td>
